01faa9aaae101da22e9cac5644ce2d526f505e23
Kaleb Heitzman <kaleb@jamfire.io>
2022-06-01 22:19:46 -0400

    updated multipass script

--- multipass.sh
+++ multipass.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-multipass launch docker -n docker-wordpress
+multipass launch docker -n docker-wordpress -c 2 -d 40G -m 4G
 multipass mount . docker-wordpress:/home/ubuntu/wordpress
+multipass exec docker-wordpress -- bash -c 'sudo apt-get update'
+multipass exec docker-wordpress -- bash -c 'sudo apt-get install docker-compose-plugin'
+multipass exec docker-wordpress -- bash -c 'cd /home/ubuntu/wordpress && docker compose up -d'
